4a8a9d94af0fce1709d271f3b2fa3af5be83e0cb
PedroLuaks <pedro.firmo.silva@escola.pr.gov.br>
2025-03-18 10:35:04 -0300

    Add files via upload

---
+++ SQLite.2.sql
@@ -0,0 +1,9 @@
+INSERT INTO AGV (nome, turma, idade)
+VALUES
+('Pedro', '3DS', 17),
+('Davi', '3DS', 16),
+('João', '3DS', 17),
+('Izael', '3DS', 17),
+('Kaio', '3DS', 17),
+('Nickolas', '3DS', 17),
+('Italo', '3DS', 17);
